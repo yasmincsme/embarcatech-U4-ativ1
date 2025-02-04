fd0d23bfe2ce4088b69a760900a231f5e2ee8c9c
Yasmin Cordeiro <ymeira@ecomp.uefs.br>
2025-02-04 01:48:21 -0300

    feat: adição dos outros leds

--- freq_03.c
+++ freq_03.c
@@ -1,42 +1,63 @@
 #include <stdio.h>
-#include "pico/stdlib.h"   // Inclui a biblioteca padrão para funcionalidades básicas como GPIO, temporização e comunicação serial.
-#include "hardware/timer.h" // Inclui a biblioteca para gerenciamento de temporizadores de hardware.
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/timer.h" 
 
-#define LED_PIN_RED 11
-bool led_on = false;
+#define LEDS 3
 
-// Função de callback que será chamada repetidamente pelo temporizador
-// O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
-bool repeating_timer_callback(struct repeating_timer *t) {
-    // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
-    printf("1 segundo passou\n");
-    //Liga ou desliga o led.
-    led_on = !led_on;
-    gpio_put(LED_PIN_RED,led_on);
-    // Retorna true para manter o temporizador repetindo. Se retornar false, o temporizador para.
+uint8_t led_pins[LEDS] = {13, 12, 11};
+
+static int current_led = 0;
+
+void setup_all(void) {
+    for (uint8_t i = 0; i < LEDS; ++i) {
+        gpio_init(led_pins[i]);
+        gpio_set_dir(led_pins[i], GPIO_OUT);
+        gpio_put(led_pins[i], 0);
+    }
+}
+
+bool timer_callback(struct repeating_timer *rt) {
+    //static int current_led = 0;
+
+    for (uint8_t i = 0; i < LEDS; ++i) {
+        gpio_put(led_pins[i], 0);
+    }
+
+    if(current_led == 1) {
+         gpio_put(led_pins[0], 1);
+         gpio_put(led_pins[2], 1);
+    }
+    else {
+        gpio_put(led_pins[current_led], 1);
+    }
+
+    current_led = (current_led + 1) % LEDS;
+
     return true;
 }
 
 int main() {
-    // Inicializa a comunicação serial, permitindo o uso de funções como printf.
     stdio_init_all();
 
-    // Inicializar o pino GPIO11
-    gpio_init(LED_PIN_RED);
-    gpio_set_dir(LED_PIN_RED,true);
+    setup_all();
 
-    // Declaração de uma estrutura de temporizador de repetição.
-    // Esta estrutura armazenará informações sobre o temporizador configurado.
     struct repeating_timer timer;
+    int32_t delay_ms = 3000;
 
-    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
-    add_repeating_timer_ms(1000, repeating_timer_callback, NULL, &timer);
-
-    // Loop infinito que mantém o programa em execução.
-    while (true) {
-        // Pausa de 10 segundos para reduzir o uso da CPU.
-        sleep_ms(10000);
-        printf("rotina de repetição\n");
+    if (!add_repeating_timer_ms(delay_ms, timer_callback, NULL, &timer)) {
+        printf("Erro ao iniciar o timer!\n");
     }
+
+    printf("Inicialização do programa em andamento...\n");
+
+    while (1) {
+        printf("Programa em execução\n");
+
+        sleep_ms(1000); 
+    }
+
     return 0;
 }
+
+//threads ou temporização?
